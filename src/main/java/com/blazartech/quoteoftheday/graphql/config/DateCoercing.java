6cd284570699583b7290fac270f19363655e76a3
Scott Aaron <dr_saaron@yahoo.com>
2023-10-13 12:46:06 -0500

    resolve deprecation warnings

--- src/main/java/com/blazartech/quoteoftheday/graphql/config/DateCoercing.java
+++ src/main/java/com/blazartech/quoteoftheday/graphql/config/DateCoercing.java
@@ -5,9 +5,14 @@
 package com.blazartech.quoteoftheday.graphql.config;
 
 import com.blazartech.products.services.date.DateServices;
+import graphql.GraphQLContext;
+import graphql.execution.CoercedVariables;
 import graphql.language.StringValue;
+import graphql.language.Value;
 import graphql.schema.Coercing;
+import graphql.schema.CoercingParseLiteralException;
 import java.util.Date;
+import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,13 +27,13 @@ public class DateCoercing implements Coercing<Date, String> {
     private DateServices dateServices;
 
     @Override
-    public String serialize(Object o) {
+    public String serialize(Object o, GraphQLContext graphQLContext, Locale locale) {
         Date d = (Date) o;
         return dateServices.formatDate(d);
     }
 
     @Override
-    public Date parseValue(Object o) {
+    public Date parseValue(Object o, GraphQLContext graphQLContext, Locale locale) {
         if (o != null) {
             String d = (String) o;
             return dateServices.parseDate(d);
@@ -36,12 +41,12 @@ public class DateCoercing implements Coercing<Date, String> {
             return null;
         }
     }
-    
+
     @Override
-    public Date parseLiteral(Object o) {
-        if (o != null) {
+    public Date parseLiteral(Value<?> o, CoercedVariables variables, GraphQLContext graphQLContext, Locale locale) throws CoercingParseLiteralException {
+         if (o != null) {
             StringValue sv = (StringValue) o;
-            return parseValue(sv.getValue());
+            return parseValue(sv.getValue(), graphQLContext, locale);
         } else {
             return null;
         }
