be48b4ecf681dc5542a0b612e256716091e0af30
Scott Aaron <dr_saaron@yahoo.com>
2025-01-16 11:21:11 -0600

    resolve mockito deprecation

--- src/test/java/com/blazartech/quoteoftheday/graphql/QueryControllerTest.java
+++ src/test/java/com/blazartech/quoteoftheday/graphql/QueryControllerTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.test.context.jdbc.Sql;
 
